1a046a48330638e7a8e446cc94366973a445eaa9
chusu91 <79313766+chusu91@users.noreply.github.com>
2022-01-20 13:59:31 +0200

    JS: Home contact btn

--- main.js
+++ main.js
@@ -12,16 +12,26 @@ window.addEventListener("scroll", () => {
 });
 
 //Handle scrolling when tapping on the navbar menu
+//html에 큰 섹션들은 아이디 지정, navbar menu item li에 data-link추가해서 아이디를 거기에 적음
 const navBarMenu = document.querySelector(".navbar__menu");
 
 navBarMenu.addEventListener("click", (event) => {
   console.log(event.target.dataset.link);
   const target = event.target;
   const link = target.dataset.link;
-
   if (link == null) {
     return;
   }
-  const scrollTo = document.querySelector(link);
-  scrollTo.scrollIntoView({ behavior: "smooth" });
+  scrollIntoView(link);
 });
+
+// Handle click on "Contact me" button on home
+const homeContactBtn = document.querySelector(".home__contact");
+homeContactBtn.addEventListener("click", () => {
+  scrollIntoView("#contact");
+});
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+}
