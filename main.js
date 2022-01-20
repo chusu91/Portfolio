0d9f5a00015a4bc91d361b1a9eef5dc72b918cb8
chusu91 <79313766+chusu91@users.noreply.github.com>
2022-01-20 15:19:49 +0200

    JS: Arrow up

--- main.js
+++ main.js
@@ -34,8 +34,25 @@ homeContactBtn.addEventListener("click", () => {
 // Make home contents slowly fade to transparent as the window scrolls down
 const homeContainer = document.querySelector(".home__container");
 const homeHeight = homeContainer.getBoundingClientRect().height;
+
 document.addEventListener("scroll", () => {
   homeContainer.style.opacity = 1 - window.scrollY / homeHeight;
+  console.log(window.scrollY);
+});
+
+// Show "arrow up" button when scrolling down
+const arrowUp = document.querySelector(".arrow-up");
+document.addEventListener("scroll", () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUp.classList.add("visible");
+  } else {
+    arrowUp.classList.remove("visible");
+  }
+});
+
+// Handle click on the "arrow up" button
+arrowUp.addEventListener("click", () => {
+  scrollIntoView("#home");
 });
 
 function scrollIntoView(selector) {
